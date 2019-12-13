3535bac5cb219a3a433c011e5ffa1a862fa73991
4311 Swampscott Currents <robotics@swampscott.k12.ma.us>
2019-12-13 16:13:55 -0500

    Updates to the IO code, including bugfixing for the angle-maintaining system.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,24 +10,39 @@ public class Robot extends FastRobot {
     private boolean isInTurnMode = false;
 
     public void robotStart() {
+        System.out.println("boib");
         RobotMap.gyroscope.reset();
     }
 
     public void teleopStart() {
+        System.out.println("sta");
         heldAngle = RobotMap.gyroscope.getAngle();
     }
 
     public void teleopUpdate() {
         if(isInTurnMode) {
-            RobotMap.driveController.driveCartesian(RobotMap.MainJoystick.getY(), 0, RobotMap.MainJoystick.getX());
+            //RobotMap.driveController.driveCartesian(RobotMap.MainJoystick.getY(), 0, RobotMap.MainJoystick.getX());
         }
         else {
-            double error = (heldAngle - RobotMap.gyroscope.getAngle()) / 180;
-            RobotMap.driveController.driveCartesian(RobotMap.MainJoystick.getY(), RobotMap.MainJoystick.getX(), clamp(0.05 * Math.signum(error) * (error * error), -0.3, 0.3), RobotMap.gyroscope.getAngle());
+            if(RobotMap.MainJoystick.getRawButton(6)) {
+                RobotMap.driveController.driveCartesian(RobotMap.MainJoystick.getX(), RobotMap.MainJoystick.getY(), 0.4, RobotMap.gyroscope.getAngle());
+                heldAngle = RobotMap.gyroscope.getAngle();
+            } else if(RobotMap.MainJoystick.getRawButton(7)) {
+                RobotMap.driveController.driveCartesian(RobotMap.MainJoystick.getX(), RobotMap.MainJoystick.getY(), -0.4, RobotMap.gyroscope.getAngle());
+                heldAngle = RobotMap.gyroscope.getAngle();
+            }
+            else {
+                double error = 1.75 * (heldAngle - RobotMap.gyroscope.getAngle()) / 180;
+                RobotMap.driveController.driveCartesian(RobotMap.MainJoystick.getX(), RobotMap.MainJoystick.getY(), clamp(12 * Math.signum(error) * (error * error), -0.3, 0.3), RobotMap.gyroscope.getAngle());
+            }
         }
         if(RobotMap.MainJoystick.getRawButtonPressed(1)) {
             isInTurnMode = !isInTurnMode;
             heldAngle = RobotMap.gyroscope.getAngle();
+        }
+        if(RobotMap.MainJoystick.getRawButton(2)) {
+            double error = (heldAngle - RobotMap.gyroscope.getAngle()) / 180;
+            System.out.println("held " + String.valueOf(clamp(12 * Math.signum(error) * (error * error), -0.3, 0.3)) + " " + String.valueOf(RobotMap.gyroscope.getAngle()));
         }
     }
 
